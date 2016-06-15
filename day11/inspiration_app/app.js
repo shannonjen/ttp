7e9b8c4a7fb497eaac06cfcca48ae47a62de5333
Jennifer Shannon <jen.shannon@gmail.com>
2016-06-15 12:35:44 -0400

    Class fix

--- day11/inspiration_app/app.js
+++ day11/inspiration_app/app.js
@@ -1,35 +1,47 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-
+var bodyp = require('body-parser');
 var app = express();
 var quotes = ["You are inspired when you find inspiration", "If you seek inspiration, look for it", "I am always inspired by inspirational quotes"];
 
+//notice the option object uses extended not extend
+app.use(bodyp.urlencoded({extended:true}));
+
+//declare location for static assets
 app.use(express.static('public'));
+
+//handlebars template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//passes in an empty string for username local variable
+//see home view
 app.get('/', function (req, res) {
-  res.render('home');
+  res.render('home', { username: ""});
 });
+
 app.get('/about', function (req, res) {
   res.render('about');
 });
+
 app.get('/inspiration', function (req, res) {
 	var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
 	res.render('inspiration', { inspirational: randomQuote });
   
 });
-// 404 catch-all handler (middleware)
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
-});
 
-// 500 error handler (middleware)
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+app.get('/login', function (req, res) {
+	var username = req.query.username;
+	var password = req.query.password;
+	console.log(req.query.username);
+	console.log(req.query.password);
+	res.render('home', { username: username });
+})
+
+app.post('/', function (req, res){
+	console.log("Hello this is a post request");
+	console.log("username: "+ req.body.username);
+	res.render('home');
 });
 
 app.listen(3000, function () {
